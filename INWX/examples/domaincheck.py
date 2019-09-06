afd1a01f06cba88678b05f5cde77d8a1a1831f14
Nick Ufer <me@nick-ufer.de>
2019-09-06 11:33:45 +0200

    Renames Domrobot class to ApiClient

--- INWX/examples/domaincheck.py
+++ INWX/examples/domaincheck.py
@@ -1,25 +1,25 @@
-from INWX.Domrobot import Domrobot
+from INWX.Domrobot import ApiClient
 
 username = ''
 password = ''
 domain = 'my-test-domain-which-is-definitely-not-registered6737.com'
 
-# By default you Domrobot uses the test api (OT&E). If you want to use the production/live api
-# we have a constant named API_LIVE_URL in the Domrobot class. Just set api_url=Domrobot.API_LIVE_URL and you're good.
+# By default you ApiClient uses the test api (OT&E). If you want to use the production/live api
+# we have a constant named API_LIVE_URL in the ApiClient class. Just set api_url=ApiClient.API_LIVE_URL and you're good.
 # You can also choose between XML-RPC and JSON-RPC by setting api_type=ApiType.XML_RPC or api_type=ApiType.JSON_RPC
-domrobot = Domrobot(api_url=Domrobot.API_OTE_URL, debug_mode=True)
+api_client = ApiClient(api_url=ApiClient.API_OTE_URL, debug_mode=True)
 
-# If you have 2fa enabled, take a look at the documentation of the Domrobot#login method to get further
+# If you have 2fa enabled, take a look at the documentation of the ApiClient#login method to get further
 # information about the login, especially the shared_secret parameter.
-login_result = domrobot.login(username, password)
+login_result = api_client.login(username, password)
 
 # login was successful
 if login_result['code'] == 1000:
 
     # Make an api call and save the result in a variable.
     # We want to check if a domain is available, so we call the api method 'domain.check'.
-    # Domrobot#call_api returns the api response as a dict.
-    domain_check_result = domrobot.call_api(api_method='domain.check', method_params={'domain': domain})
+    # ApiClient#call_api returns the api response as a dict.
+    domain_check_result = api_client.call_api(api_method='domain.check', method_params={'domain': domain})
 
     # request was successful
     if domain_check_result['code'] == 1000:
@@ -37,6 +37,6 @@ if login_result['code'] == 1000:
                         + '  Message: ' + domain_check_result['msg'])
 
     # With or without successful check, we perform a logout.
-    domrobot.logout()
+    api_client.logout()
 else:
     raise Exception('Api login error. Code: ' + login_result['code'] + '  Message: ' + login_result['msg'])
--- README.md
+++ README.md
@@ -25,18 +25,18 @@ Example
 -------
 
 ```python
-from domrobot import Domrobot
+from INWX.Domrobot import ApiClient
 
 username = ''
 password = ''
 domain = 'my-test-domain-which-is-definitely-not-registered6737.com'
 
-domrobot = Domrobot(api_url=Domrobot.API_OTE_URL, debug_mode=True)
+api_client = ApiClient(api_url=ApiClient.API_OTE_URL, debug_mode=True)
 
-login_result = domrobot.login(username, password)
+login_result = api_client.login(username, password)
 
 if login_result['code'] == 1000:
-    domain_check_result = domrobot.call_api(api_method='domain.check', method_params={'domain': domain})
+    domain_check_result = api_client.call_api(api_method='domain.check', method_params={'domain': domain})
 
     if domain_check_result['code'] == 1000:
         checked_domain = domain_check_result['resData']['domain'][0]
@@ -49,7 +49,7 @@ if login_result['code'] == 1000:
     else:
         raise Exception('Api error while checking domain status. Code: ' + domain_check_result['code']
                         + '  Message: ' + domain_check_result['msg'])
-    domrobot.logout()
+    api_client.logout()
 else:
     raise Exception('Api login error. Code: ' + login_result['code'] + '  Message: ' + login_result['msg'])
 ```
